d2c0cfdbe9ec8b2774645b7be88b9ff6877c9df9
Allison Ause <allison.ause@gmail.com>
2022-06-01 15:40:05 -0700

    Verified button shows animal section, clicking the button successfully unhides the animal image and additional options

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // grab DOM elements
 const tosCheckbox = document.getElementById('tos-checkbox');
 const showAnswerButton = document.getElementById('show-answer-button');
+const animalSection = document.getElementById('animal-section');
 
 // set event listeners
     // get info from user input
@@ -9,4 +10,8 @@ const showAnswerButton = document.getElementById('show-answer-button');
 
 tosCheckbox.addEventListener('change', () => {
     showAnswerButton.disabled = !tosCheckbox.checked;
+});
+
+showAnswerButton.addEventListener('click', () => {
+    animalSection.classList.remove('hidden');
 });
