ed3bbfceb74e2f62e54c20a71a82a641fa02c72c
Allison Ause <allison.ause@gmail.com>
2022-06-01 15:28:09 -0700

    TOS enables button, when I click the box in the live server, the answer box is revealed successfully

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // grab DOM elements
+const tosCheckbox = document.getElementById('tos-checkbox');
+const showAnswerButton = document.getElementById('show-answer-button');
 
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
+
+tosCheckbox.addEventListener('change', () => {
+    showAnswerButton.disabled = !tosCheckbox.checked;
+});
